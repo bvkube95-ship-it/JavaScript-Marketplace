2ca96a00491477fc27e14da076459662949a1ffe
Exider <a0242136@gmail.com>
2026-05-28 16:35:19 +0200

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -14,9 +14,9 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
               <img class="product-rating-stars"
-                src="images/ratings/rating-${product.raiting.stars * 10}.png">
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
               <div class="product-rating-count link-primary">
-                ${product.raiting.count}
+                ${product.rating.count}
               </div>
             </div>
 
@@ -46,14 +46,46 @@ products.forEach((product) => {
               Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
               Add to Cart
             </button>
           </div>
   `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+        productId: productId,
+        quantity: 1
+      });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+    });
+  });
