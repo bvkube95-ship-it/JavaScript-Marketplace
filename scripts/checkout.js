f1d084c323aed67283ac6aff937ae3062a323091
Exider <a0242136@gmail.com>
2026-05-30 01:23:54 +0200

    make a formatCurrency fumction

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = '';
 
@@ -24,7 +25,7 @@ cart.forEach((cartItem) => {
               ${matchingItem.name}
             </div>
             <div class="product-price">
-              $${(matchingItem.priceCents / 100).toFixed(2)}
+              $${formatCurrency(matchingItem.priceCents)}
             </div>
             <div class="product-quantity">
               <span>
