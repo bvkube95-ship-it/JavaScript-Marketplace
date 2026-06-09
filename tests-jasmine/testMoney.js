e1ccb956ebed56303673dcfac3d2950c515a41b4
Exider <a0242136@gmail.com>
2026-06-09 19:11:29 +0200

    Add money test in jasmine

---
+++ tests-jasmine/testMoney.js
@@ -0,0 +1,15 @@
+import formatCurrency from '../scripts/utils/money.js';
+
+describe("A suite is just a function", () => {
+  it('converts cents', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('work woth zero', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds to cent', () => {
+    expect(formatCurrency(2005.5)).toEqual('20.06');
+  });
+});
