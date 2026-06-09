879f4ad4c542e25bd4ec9ad5de9e4da29ae53e0d
Exider <a0242136@gmail.com>
2026-06-09 15:26:37 +0200

    Fixed format currency

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { calculateOrder, renderPaymentSummary, updateQuantity } from "./checkout/paymentSummary.js";
 import renderHeader from "./checkout/checkoutHeader.js";
+import formatCurrency from "./utils/money.js";
 
 renderOrderSummary();
 updateQuantity();
