5fb577bfbefa96b0fc31957f3f0694a3206c386b
Exider <a0242136@gmail.com>
2026-06-11 14:54:12 +0200

    Make the price and stars using class

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -83,7 +83,7 @@ export function renderOrderSummary() {
               ${matchingItem.name}
             </div>
             <div class="product-price">
-              $${formatCurrency(matchingItem.priceCents)}
+              ${matchingItem.getPrice()}
             </div>
             <div class="product-quantity">
               <span>
