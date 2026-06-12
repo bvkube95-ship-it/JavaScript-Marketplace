285fd34cfc317154c9286310ff39ea57364d075e
Exider <a0242136@gmail.com>
2026-06-12 18:35:46 +0200

    Make product inheritance

--- data/products.js
+++ data/products.js
@@ -19,11 +19,32 @@ class Product {
 
   getStarsUrl() {
   return `images/ratings/rating-${this.rating.stars * 10}.png`
-}
+  }
 
   getPrice() {
   return `$${formatCurrency(this.priceCents)}`
+  }
+
+  extraInfoHTML() {
+    return ``;
+  }
 }
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails)
+    this.sizeChartLink = productDetails.sizeChartLink
+  }
+
+  extraInfoHTML() {
+  return `
+    <a href="${this.sizeChartLink}" target="_blank">
+      Size chart
+    </a>
+  `;
+  }
 }
 
 export const products = [
@@ -686,7 +707,10 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
-  return new Product(productDetails)
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  } 
+    return new Product(productDetails)
 });
 
 products.forEach((product) => {
