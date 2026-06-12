5f0d5e121506a10ff111dd5600e49bc0dde89ed3
Exider <a0242136@gmail.com>
2026-06-12 19:57:07 +0200

    backend-practice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,9 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { calculateOrder, renderPaymentSummary, updateQuantity } from "./checkout/paymentSummary.js";
 import renderHeader from "./checkout/checkoutHeader.js";
 import formatCurrency from "./utils/money.js";
-
 import { cart } from "../data/cart-class.js";
+
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 updateQuantity();
