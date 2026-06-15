b28926ac8ab1b2fa7f4bedf6b6c394f53ae5193e
Exider <a0242136@gmail.com>
2026-06-15 23:50:07 +0200

    Make "dont ask again" checkbox

--- scripts/orders/popup.js
+++ scripts/orders/popup.js
@@ -1,20 +1,31 @@
+import { productsMap } from "../../data/products.js";
+
 const popup = document.querySelector('.js-popup');
 const overlay = document.querySelector('.js-popup-overlay');
 const closeBtn = document.querySelector('.js-popup-close');
+const dontAsk = document.querySelector('.js-dont-ask');
+const confirmBtn = document.querySelector('.popup-btn--confirm');
 
 export function showPopup() {
+  if (sessionStorage.getItem('dontAskCart') === 'true') {
+    window.location.href = 'checkout.html';
+    return;
+  }
   popup.style.display = 'block';
   overlay.style.display = 'block';
-
-  popup.addEventListener('click', () => {
-    window.location.href = 'checkout.html'
-  })
 }
 
 function closePopup() {
+  if (dontAsk.checked) {
+    sessionStorage.setItem('dontAskCart', 'true');
+  }
   popup.style.display = 'none';
   overlay.style.display = 'none';
 }
 
+confirmBtn.addEventListener('click', () => {
+  closePopup();
+  window.location.href = 'checkout.html';
+});
 closeBtn.addEventListener('click', closePopup);
 overlay.addEventListener('click', closePopup);
